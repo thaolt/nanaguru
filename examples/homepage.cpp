abfa76108d3f378b8aa3d6c69785813b1a243918
Thao Le <thaolt@songphi.com>
2015-11-18 03:31:10 +0700

    initial import

---
+++ examples/homepage.cpp
@@ -0,0 +1,13 @@
+#include<nana/gui.hpp>
+
+int main()
+{
+     using namespace nana;
+     form fm;
+     drawing{fm}.draw([](paint::graphics& graph){
+         graph.string({10, 10}, L"Hello, world!", colors::red);
+     });
+     fm.events().click(API::exit);
+     fm.show();
+     exec();
+}
